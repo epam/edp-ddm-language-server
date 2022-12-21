a82d7bb1b0264ae790668f5b17c5263ef1d3e7b0
Eduard_Kuznetsov <eduard_kuznetsov@epam.com>
2022-12-21 12:11:02 +0200

    [EDP-DDM-22344] added copyright and ddm-starter-logger
    Change-Id: I5aebe96e1add0bf4ebdd370bf5fee337c41a670c

--- src/main/java/com/epam/digital/data/platform/langserver/ddmlanguageserver/handler/GroovyWebSocketHandler.java
+++ src/main/java/com/epam/digital/data/platform/langserver/ddmlanguageserver/handler/GroovyWebSocketHandler.java
@@ -1,82 +1,98 @@
+/*
+ * Copyright 2022 EPAM Systems.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.epam.digital.data.platform.langserver.ddmlanguageserver.handler;
 
 import com.epam.digital.data.platform.langserver.ddmlanguageserver.exception.WebSocketConnectionException;
 import com.epam.digital.data.platform.langserver.ddmlanguageserver.exception.WebSocketException;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import lombok.extern.slf4j.Slf4j;
 import net.prominic.groovyls.GroovyLanguageServer;
 import org.eclipse.lsp4j.jsonrpc.RemoteEndpoint;
 import org.eclipse.lsp4j.jsonrpc.json.MessageJsonHandler;
 import org.eclipse.lsp4j.jsonrpc.services.ServiceEndpoints;
 import org.eclipse.lsp4j.services.LanguageClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.SubProtocolCapable;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+@Slf4j
+@Component
 public class GroovyWebSocketHandler extends TextWebSocketHandler implements SubProtocolCapable {
-
-  private static final Logger logger = LoggerFactory.getLogger(GroovyWebSocketHandler.class);
 
   private final Map<WebSocketSession, RemoteEndpoint> servers = new ConcurrentHashMap<>();
 
   private final MessageJsonHandler messageJsonHandler = new MessageJsonHandler(
       ServiceEndpoints.getSupportedMethods(GroovyLanguageServer.class));
 
-
   @Override
-  public void afterConnectionEstablished(WebSocketSession session) {
-    logger.info("Server connection opened");
-    GroovyLanguageServer groovyLanguageServer = new GroovyLanguageServer();
-    RemoteEndpoint remoteEndpoint = new RemoteEndpoint(message -> {
-      String json = messageJsonHandler.serialize(message);
-      logger.info("Supposed output: {}", json);
+  public void afterConnectionEstablished(@NonNull WebSocketSession session) {
+    log.info("Server connection opened");
+    var groovyLanguageServer = new GroovyLanguageServer();
+    var remoteEndpoint = new RemoteEndpoint(message -> {
+      var json = messageJsonHandler.serialize(message);
+      log.trace("Output message: {}", json);
       try {
         if (session.isOpen()) {
           session.sendMessage(new TextMessage(json));
         } else {
-          logger.error("Session is closed");
+          log.error("Session is closed");
           throw new WebSocketConnectionException("Session is closed");
         }
       } catch (IOException e) {
-        logger.error("Exception occurs during sending message");
+        log.error("Exception occurs during sending message");
         throw new WebSocketException("Exception occurs during sending message", e);
       }
 
     }, ServiceEndpoints.toEndpoint(groovyLanguageServer));
-    LanguageClient languageClient = ServiceEndpoints.toServiceObject(remoteEndpoint,
-        LanguageClient.class);
+    var languageClient = ServiceEndpoints.toServiceObject(remoteEndpoint, LanguageClient.class);
     groovyLanguageServer.connect(languageClient);
 
     servers.put(session, remoteEndpoint);
   }
 
   @Override
-  public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-    logger.info("Server connection closed: {}", status);
+  public void afterConnectionClosed(@NonNull WebSocketSession session,
+      @NonNull CloseStatus status) {
+    log.info("Server connection closed: {}", status);
     servers.remove(session);
   }
 
   @Override
-  public void handleTextMessage(WebSocketSession session, TextMessage message) {
-    String request = message.getPayload();
-    logger.info("input: {}", request);
+  public void handleTextMessage(@NonNull WebSocketSession session, TextMessage message) {
+    var request = message.getPayload();
+    log.trace("Input message: {}", request);
     servers.get(session).consume(messageJsonHandler.parseMessage(request));
   }
 
   @Override
-  public void handleTransportError(WebSocketSession session, Throwable exception) {
-    logger.error("Server transport error: {}", exception.getMessage());
+  public void handleTransportError(@NonNull WebSocketSession session, Throwable exception) {
+    log.error("Server transport error: {}", exception.getMessage());
   }
 
   @Override
+  @NonNull
   public List<String> getSubProtocols() {
-    return Collections.singletonList("com.epam.digital.data.platform.langserver");
+    return List.of("com.epam.digital.data.platform.langserver");
   }
 }
--- src/test/java/com/epam/digital/data/platform/langserver/ddmlanguageserver/DdmLanguageServerApplicationTests.java
+++ src/test/java/com/epam/digital/data/platform/langserver/ddmlanguageserver/DdmLanguageServerApplicationTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 EPAM Systems.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.epam.digital.data.platform.langserver.ddmlanguageserver;
 
 import static org.mockito.ArgumentMatchers.eq;
@@ -24,60 +40,61 @@ import org.springframework.web.socket.WebSocketSession;
 @ExtendWith(SpringExtension.class)
 class DdmLanguageServerApplicationTests {
 
-	@Mock
-	WebSocketSession session;
-	@Mock
-	RemoteEndpoint remoteEndpoint;
-	@Mock
-	Map<WebSocketSession, RemoteEndpoint> servers;
-	@Mock
-	MessageJsonHandler messageJsonHandler;
-	@InjectMocks
-	GroovyWebSocketHandler groovyWebSocketHandler;
+  @Mock
+  WebSocketSession session;
+  @Mock
+  RemoteEndpoint remoteEndpoint;
+  @Mock
+  Map<WebSocketSession, RemoteEndpoint> servers;
+  @Mock
+  MessageJsonHandler messageJsonHandler;
+  @InjectMocks
+  GroovyWebSocketHandler groovyWebSocketHandler;
 
-	@BeforeEach
-	void setup() {
-		ReflectionTestUtils.setField(groovyWebSocketHandler, "servers", servers);
-		ReflectionTestUtils.setField(groovyWebSocketHandler, "messageJsonHandler", messageJsonHandler);
-	}
+  @BeforeEach
+  void setup() {
+    ReflectionTestUtils.setField(groovyWebSocketHandler, "servers", servers);
+    ReflectionTestUtils.setField(groovyWebSocketHandler, "messageJsonHandler", messageJsonHandler);
+  }
 
-	@Test
-	void afterConnectionEstablishedTest() {
-		groovyWebSocketHandler.afterConnectionEstablished(session);
-		Mockito.verify(servers).put(eq(session), Mockito.any(RemoteEndpoint.class));
-	}
+  @Test
+  void afterConnectionEstablishedTest() {
+    groovyWebSocketHandler.afterConnectionEstablished(session);
+    Mockito.verify(servers).put(eq(session), Mockito.any(RemoteEndpoint.class));
+  }
 
-	@Test
-	void afterConnectionClosedTest() {
-		groovyWebSocketHandler.afterConnectionClosed(session, CloseStatus.BAD_DATA);
-		Mockito.verify(servers).remove(session);
-	}
+  @Test
+  void afterConnectionClosedTest() {
+    groovyWebSocketHandler.afterConnectionClosed(session, CloseStatus.BAD_DATA);
+    Mockito.verify(servers).remove(session);
+  }
 
-	@Test
-	void handleTextMessageTest() {
-		var textMessage = new TextMessage(RandomString.make());
-		Mockito.when(servers.get(session)).thenReturn(remoteEndpoint);
-		Mockito.when(messageJsonHandler.parseMessage(textMessage.getPayload()))
-				.thenReturn(new RequestMessage());
-		groovyWebSocketHandler.handleTextMessage(session, textMessage);
+  @Test
+  void handleTextMessageTest() {
+    var textMessage = new TextMessage(RandomString.make());
+    Mockito.when(servers.get(session)).thenReturn(remoteEndpoint);
+    Mockito.when(messageJsonHandler.parseMessage(textMessage.getPayload()))
+        .thenReturn(new RequestMessage());
+    groovyWebSocketHandler.handleTextMessage(session, textMessage);
 
-		Mockito.verify(remoteEndpoint)
-				.consume(messageJsonHandler.parseMessage(textMessage.getPayload()));
-	}
+    Mockito.verify(remoteEndpoint)
+        .consume(messageJsonHandler.parseMessage(textMessage.getPayload()));
+  }
 
-	@Test
-	void handleTransportErrorDoNotThrowAnyExceptions() {
-		Assertions.assertThatCode(
-				() -> groovyWebSocketHandler.handleTransportError(session, new Exception()))
-				.doesNotThrowAnyException();
-	}
+  @Test
+  void handleTransportErrorDoNotThrowAnyExceptions() {
+    Assertions.assertThatCode(
+            () -> groovyWebSocketHandler.handleTransportError(session, new Exception()))
+        .doesNotThrowAnyException();
+  }
 
-	@Test
-	void subProtocolsTest() {
-		var subProtocols = groovyWebSocketHandler.getSubProtocols();
-		Assertions.assertThat(subProtocols).isNotNull();
-		Assertions.assertThat(subProtocols).isNotEmpty();
-		Assertions.assertThat(subProtocols.get(0)).isEqualTo("com.epam.digital.data.platform.langserver");
-	}
+  @Test
+  void subProtocolsTest() {
+    var subProtocols = groovyWebSocketHandler.getSubProtocols();
+    Assertions.assertThat(subProtocols).isNotNull();
+    Assertions.assertThat(subProtocols).isNotEmpty();
+    Assertions.assertThat(subProtocols.get(0))
+        .isEqualTo("com.epam.digital.data.platform.langserver");
+  }
 
 }
